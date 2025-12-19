60487e77380bc733747bae02b216e3408ecd3b1f
alvaro.junqueira <alvaro.alf2@gmail.com>
2025-12-19 09:06:45 -0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,12 +9,12 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<Persistence.AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Build the application.
-
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 
